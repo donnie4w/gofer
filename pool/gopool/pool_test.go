4c7e16906426b6e46af754d46a90bcda2efc3fd8
donnie <donnie4w@gmail.com>
2023-07-14 17:02:22 +0800

    Create pool_test.go

---
+++ pool/gopool/pool_test.go
@@ -0,0 +1,62 @@
+package gopool
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Benchmark_gopool(b *testing.B) {
+	b.StopTimer()
+	pool := NewPool(100, 100)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		for k := 0; k < 10000; k++ {
+			pool.Go(func() {
+				s := ""
+				for i := 0; i < 10; i++ {
+					s = fmt.Sprint(s, i)
+				}
+			})
+		}
+	}
+}
+
+func Benchmark_gof(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for k := 0; k < 10000; k++ {
+			go func() {
+				s := ""
+				for i := 0; i < 10; i++ {
+					s = fmt.Sprint(s, i)
+				}
+			}()
+		}
+	}
+}
+
+func BenchmarkParallel_gopool(b *testing.B) {
+	pool := NewPool(100, 100)
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			pool.Go(func() {
+				s := ""
+				for i := 0; i < 10; i++ {
+					s = fmt.Sprint(s, i)
+				}
+			})
+		}
+	})
+}
+
+func BenchmarkParallel_gof(b *testing.B) {
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			go func() {
+				s := ""
+				for i := 0; i < 10; i++ {
+					s = fmt.Sprint(s, i)
+				}
+			}()
+		}
+	})
+}
