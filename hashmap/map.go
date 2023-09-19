d02511cd41cc91ac204877382d8aa1e91aa8d531
donnie <donnie4w@gmail.com>
2023-09-20 00:01:54 +0800

    Update map.go

--- hashmap/map.go
+++ hashmap/map.go
@@ -1,7 +1,8 @@
 // Copyright (c) , donnie <donnie4w@gmail.com>
 // All rights reserved.
 //
-// github.com/donnie4w/gofer
+// github.com/donnie4w/gofer/hashmap
+
 package hashmap
 
 import (
@@ -12,11 +13,10 @@ import (
 type MapL[K any, V any] struct {
 	m   sync.Map
 	len int64
-	mux *sync.Mutex
 }
 
 func NewMapL[K any, V any]() *MapL[K, V] {
-	return &MapL[K, V]{m: sync.Map{}, mux: &sync.Mutex{}}
+	return &MapL[K, V]{m: sync.Map{}}
 }
 
 func (this *MapL[K, V]) Put(key K, value V) {
@@ -27,7 +27,10 @@ func (this *MapL[K, V]) Put(key K, value V) {
 
 func (this *MapL[K, V]) Get(key K) (_r V, b bool) {
 	if v, ok := this.m.Load(key); ok {
-		_r, b = v.(V), ok
+		if v != nil {
+			_r = v.(V)
+		}
+		b = true
 	}
 	return
 }
@@ -37,17 +40,21 @@ func (this *MapL[K, V]) Has(key K) (ok bool) {
 	return
 }
 
-func (this *MapL[K, V]) Del(key K) {
-	this.mux.Lock()
-	defer this.mux.Unlock()
-	if _, ok := this.m.LoadAndDelete(key); ok {
+func (this *MapL[K, V]) Del(key K) (ok bool) {
+	if _, ok = this.m.LoadAndDelete(key); ok {
 		atomic.AddInt64(&this.len, -1)
 	}
+	return
 }
 
 func (this *MapL[K, V]) Range(f func(k K, v V) bool) {
 	this.m.Range(func(k, v any) bool {
-		return f(k.(K), v.(V))
+		if v != nil {
+			return f(k.(K), v.(V))
+		} else {
+			var t V
+			return f(k.(K), t)
+		}
 	})
 }
 
@@ -64,16 +71,26 @@ func NewMap[K any, V any]() *Map[K, V] {
 	return &Map[K, V]{m: sync.Map{}}
 }
 
-func (this *Map[K, V]) Put(key K, value V) (v V, loaded bool) {
-	if previous, ok := this.m.Swap(key, value); ok {
-		v, loaded = previous.(V), ok
+func (this *Map[K, V]) Put(key K, value V) {
+	this.m.Swap(key, value)
+}
+
+func (this *Map[K, V]) Swap(key K, value V) (v V, ok bool) {
+	if previous, loaded := this.m.Swap(key, value); loaded {
+		if previous != nil {
+			v = previous.(V)
+		}
+		ok = true
 	}
 	return
 }
 
-func (this *Map[K, V]) Get(key K) (_r V, b bool) {
-	if v, ok := this.m.Load(key); ok {
-		_r, b = v.(V), ok
+func (this *Map[K, V]) Get(key K) (v V, b bool) {
+	if e, ok := this.m.Load(key); ok {
+		if e != nil {
+			v = e.(V)
+		}
+		b = ok
 	}
 	return
 }
@@ -83,15 +100,18 @@ func (this *Map[K, V]) Has(key K) (ok bool) {
 	return
 }
 
-func (this *Map[K, V]) Del(key K) (v V, ok bool) {
-	if e, loaded := this.m.LoadAndDelete(key); loaded {
-		v, ok = e.(V), loaded
-	}
+func (this *Map[K, V]) Del(key K) (ok bool) {
+	_, ok = this.m.LoadAndDelete(key)
 	return
 }
 
 func (this *Map[K, V]) Range(f func(k K, v V) bool) {
 	this.m.Range(func(k, v any) bool {
-		return f(k.(K), v.(V))
+		if v != nil {
+			return f(k.(K), v.(V))
+		} else {
+			var t V
+			return f(k.(K), t)
+		}
 	})
 }
