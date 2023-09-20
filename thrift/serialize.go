03823654f4be4ccfefec1a7388d648e9c54dae0a
donnie <donnie4w@gmail.com>
2023-09-20 15:45:23 +0800

    Create serialize.go

---
+++ thrift/serialize.go
@@ -0,0 +1,26 @@
+package thrift
+
+import (
+	"bytes"
+	"context"
+
+	gothrift "github.com/apache/thrift/lib/go/thrift"
+)
+
+var tconf = &gothrift.TConfiguration{}
+
+func TEncode(ts gothrift.TStruct) (_r []byte) {
+	buf := &gothrift.TMemoryBuffer{Buffer: bytes.NewBuffer([]byte{})}
+	protocol := gothrift.NewTCompactProtocolConf(buf, tconf)
+	ts.Write(context.Background(), protocol)
+	protocol.Flush(context.Background())
+	_r = buf.Bytes()
+	return
+}
+
+func TDecode[T gothrift.TStruct](bs []byte, ts T) (_r T, err error) {
+	buf := &gothrift.TMemoryBuffer{Buffer: bytes.NewBuffer(bs)}
+	protocol := gothrift.NewTCompactProtocolConf(buf, tconf)
+	err = ts.Read(context.Background(), protocol)
+	return ts, err
+}
