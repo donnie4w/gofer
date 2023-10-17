98b24de8bd0647c7c43595a5b2e42b717679c849
donnie <donnie4w@gmail.com>
2023-10-18 01:03:45 +0800

    Create bin.go

---
+++ keystore/bin.go
@@ -0,0 +1,62 @@
+// Copyright (c) , donnie <donnie4w@gmail.com>
+// All rights reserved.
+//
+// github.com/donnie4w/tim/keystore
+
+package keystore
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"sync"
+
+	"github.com/donnie4w/gofer/util"
+)
+
+var KeyStore *_keyStore
+
+type _keyStore struct {
+	mux          *sync.Mutex
+	fname        string
+	_fileHandler *os.File
+}
+
+func NewKeyStore(dir string, name string) (ks *_keyStore, err error) {
+	fname := fmt.Sprint(dir, "/", name)
+	var _fileHandler *os.File
+	if _fileHandler, err = os.OpenFile(fname, os.O_RDWR|os.O_CREATE, 0666); err == nil {
+		ks = &_keyStore{&sync.Mutex{}, fname, _fileHandler}
+	}
+	return
+}
+
+func (this *_keyStore) Write(bs []byte) (err error) {
+	defer this.mux.Unlock()
+	this.mux.Lock()
+	this._fileHandler.Seek(0, io.SeekStart)
+	this._fileHandler.Truncate(0)
+	var obs []byte
+	if obs, err = util.Zlib(bs); err != nil {
+		obs = bs
+	}
+	_, err = this._fileHandler.Write(obs)
+	return
+}
+
+func (this *_keyStore) Read() (bs []byte, err error) {
+	defer this.mux.Unlock()
+	this.mux.Lock()
+	fi, err := this._fileHandler.Stat()
+	if fi.Size() > 0 {
+		if bs, err = util.ReadFile(this.fname); err == nil && bs != nil {
+			if obs, er := util.UnZlib(bs); er == nil {
+				return obs, er
+			}
+		}
+	} else {
+		err = errors.New("empty file")
+	}
+	return
+}
