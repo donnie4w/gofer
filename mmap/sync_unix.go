f94d913c40af56ad31736cb95645c4c3bdc4734b
donnie4w <donnie4w@gmail.com>
2024-02-26 20:50:43 +0800

    Update sync_unix.go

--- mmap/sync_unix.go
+++ mmap/sync_unix.go
@@ -4,21 +4,18 @@
 package mmap
 
 import (
-	"fmt"
 	"os"
-	"unsafe"
 
 	"golang.org/x/sys/unix"
 )
 
 func mmapSyncToDisk(file *os.File, mappedMemory []byte) (err error) {
-	ptr := unsafe.Pointer(&mappedMemory[0])
-	length := uintptr(len(mappedMemory))
-
-	_, _, errno := unix.Syscall6(unix.SYS_MSYNC, uintptr(ptr), length, unix.MS_SYNC, 0, 0, 0)
-	if errno != 0 {
-		return fmt.Errorf("msync failed: %v", errno)
-	}
-
-	return nil
+	// ptr := unsafe.Pointer(&mappedMemory[0])
+	// length := uintptr(len(mappedMemory))
+	// _, _, errno := unix.Syscall6(unix.SYS_MSYNC, uintptr(ptr), length, unix.MS_SYNC, 0, 0, 0)
+	// if errno != 0 {
+	// 	return fmt.Errorf("msync failed: %v", errno)
+	// }
+	err = unix.Msync(mappedMemory, unix.MS_SYNC)
+	return
 }
