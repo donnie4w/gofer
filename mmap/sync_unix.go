c4f808e339c0fbe866228c4383f0897813175c15
donnie4w <donnie4w@gmail.com>
2024-02-26 23:25:13 +0800

    Update sync_unix.go

--- mmap/sync_unix.go
+++ mmap/sync_unix.go
@@ -12,12 +12,17 @@ import (
 )
 
 func mmapSyncToDisk(file *os.File, mappedMemory []byte) (err error) {
-	pageSize := unix.Getpagesize()
+	pageSize := uint64(unix.Getpagesize())
+
 	ptr := unsafe.Pointer(&mappedMemory[0])
 	startOffset := uintptr(ptr)
 	length := uintptr(len(mappedMemory))
+
 	alignedStart := startOffset &^ (pageSize - 1)
-	alignedLength := (length + (startOffset - alignedStart)) &^ (pageSize - 1)
+
+	alignedEnd := (startOffset + length + pageSize - 1) &^ (pageSize - 1)
+	alignedLength := alignedEnd - alignedStart
+
 	_, _, errno := unix.Syscall6(unix.SYS_MSYNC, alignedStart, alignedLength, unix.MS_SYNC, 0, 0, 0)
 	if errno != 0 {
 		return fmt.Errorf("msync failed: %v", errno)
