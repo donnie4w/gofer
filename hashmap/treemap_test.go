7b15bca9b3851922cf56d22f674558fdd438148d
donnie4w <donnie4w@gmail.com>
2024-09-20 09:44:55 +0800

    Create treemap_test.go

---
+++ hashmap/treemap_test.go
@@ -0,0 +1,61 @@
+package hashmap
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+)
+
+func BenchmarkParalleltreeMap(b *testing.B) {
+	tm := NewTreeMap[uint64, int64](64)
+	b.ResetTimer()
+	var i int64 = 0
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			k := uint64(time.Now().UnixNano() + atomic.AddInt64(&i, 1))
+			tm.Put(k, time.Now().UnixNano())
+			if k%9 == 0 {
+				tm.Delete(k)
+			}
+			if _, ok := tm.Get(k); !ok {
+			}
+		}
+	})
+}
+
+func BenchmarkSerialLimittreeMap(b *testing.B) {
+	tm := NewTreeMap[uint64, int64](32)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		k := uint64(time.Now().UnixNano() + int64(i))
+		tm.Put(k, time.Now().UnixNano())
+		if k%9 == 0 {
+			tm.Delete(k)
+		}
+		tm.Get(k)
+	}
+}
+
+func Test_treeMap(t *testing.T) {
+	tm := NewTreeMap[int, int64](32)
+	var wg sync.WaitGroup
+	for i := 0; i < 20; i++ {
+		go func(i int) {
+			wg.Add(1)
+			defer wg.Done()
+			tm.Put(i, time.Now().UnixNano())
+		}(i)
+	}
+	wg.Wait()
+	tm.Ascend(func(i int, i2 int64) bool {
+		fmt.Println(i, ":", i2)
+		return true
+	})
+
+	tm.Descend(func(i int, i2 int64) bool {
+		fmt.Println(i, ":", i2)
+		return true
+	})
+}
