ef9d7ca84c5c91eed3e5c75808960c9049f9f3ca
donnie4w <donnie4w@gmail.com>
2024-09-20 09:44:18 +0800

    Update fast.go

--- fast/fast.go
+++ fast/fast.go
@@ -2,13 +2,14 @@
 // All rights reserved.
 //
 // github.com/donnie4w/gofer
+
 package fast
 
 import "unsafe"
 
-//the use of the unsafe package violates the principle of secure abstraction in the Go language.
-//It makes the code more susceptible to the influence of the runtime environment.
-//For example, the garbage collector may move memory locations, which can lead to unexpected errors in code that uses unsafe
+// StringToSlice the use of the unsafe package violates the principle of secure abstraction in the Go language.
+// It makes the code more susceptible to the influence of the runtime environment.
+// For example, the garbage collector may move memory locations, which can lead to unexpected errors in code that uses unsafe
 func StringToSlice(s string) []byte {
 	return unsafe.Slice(unsafe.StringData(s), len(s))
 }
