a019fb3e284ddc7d308578ea40792ed396f40d79
donnie4w <donnie4w@gmail.com>
2024-09-20 09:44:49 +0800

    Update map.go

--- hashmap/map.go
+++ hashmap/map.go
@@ -6,32 +6,31 @@
 package hashmap
 
 import (
-	"container/list"
-	"hash/crc64"
-	"sort"
 	"sync"
 	"sync/atomic"
-
-	. "github.com/donnie4w/gofer/buffer"
 )
 
+// MapL Define a generic synchronized map structure that also keeps track of its length.
+// It uses sync.Map as the underlying data structure and an int64 variable to count the number of elements.
 type MapL[K any, V any] struct {
 	m   sync.Map
 	len int64
 }
 
+// NewMapL Create a new instance of a generic MapL and return its pointer.
+// The initial length is set to 0.
 func NewMapL[K any, V any]() *MapL[K, V] {
 	return &MapL[K, V]{m: sync.Map{}}
 }
 
-func (this *MapL[K, V]) Put(key K, value V) {
-	if _, ok := this.m.Swap(key, value); !ok {
-		atomic.AddInt64(&this.len, 1)
+func (t *MapL[K, V]) Put(key K, value V) {
+	if _, ok := t.m.Swap(key, value); !ok {
+		atomic.AddInt64(&t.len, 1)
 	}
 }
 
-func (this *MapL[K, V]) Get(key K) (_r V, b bool) {
-	if v, ok := this.m.Load(key); ok {
+func (t *MapL[K, V]) Get(key K) (_r V, b bool) {
+	if v, ok := t.m.Load(key); ok {
 		if v != nil {
 			_r = v.(V)
 		}
@@ -40,20 +39,35 @@ func (this *MapL[K, V]) Get(key K) (_r V, b bool) {
 	return
 }
 
-func (this *MapL[K, V]) Has(key K) (ok bool) {
-	_, ok = this.m.Load(key)
+func (t *MapL[K, V]) Has(key K) (ok bool) {
+	_, ok = t.m.Load(key)
 	return
 }
 
-func (this *MapL[K, V]) Del(key K) (ok bool) {
-	if _, ok = this.m.LoadAndDelete(key); ok {
-		atomic.AddInt64(&this.len, -1)
+func (t *MapL[K, V]) Del(key K) (ok bool) {
+	if _, ok = t.m.LoadAndDelete(key); ok {
+		atomic.AddInt64(&t.len, -1)
 	}
 	return
 }
 
-func (this *MapL[K, V]) Range(f func(k K, v V) bool) {
-	this.m.Range(func(k, v any) bool {
+// Swap the value associated with the given key in the MapL.
+// If the key exists, return the old value and true; otherwise, return the zero value and false.
+func (t *MapL[K, V]) Swap(key K, value V) (_r V, loaded bool) {
+	var previous any
+	if previous, loaded = t.m.Swap(key, value); !loaded {
+		atomic.AddInt64(&t.len, 1)
+	} else if previous != nil {
+		_r = previous.(V)
+	}
+	return
+}
+
+// Range  Iterate over all elements in the MapL.
+// For each element, call the provided function with the key and value.
+// The iteration stops if the function returns false.
+func (t *MapL[K, V]) Range(f func(k K, v V) bool) {
+	t.m.Range(func(k, v any) bool {
 		if v != nil {
 			return f(k.(K), v.(V))
 		} else {
@@ -63,35 +77,26 @@ func (this *MapL[K, V]) Range(f func(k K, v V) bool) {
 	})
 }
 
-func (this *MapL[K, V]) Len() int64 {
-	return this.len
+func (t *MapL[K, V]) Len() int64 {
+	return t.len
 }
 
-/***********************************************************/
+// Map  Define a generic synchronized map structure using sync.Map as the underlying data structure.
 type Map[K any, V any] struct {
 	m sync.Map
 }
 
+// NewMap Create a new instance of a generic Map and return its pointer.
 func NewMap[K any, V any]() *Map[K, V] {
 	return &Map[K, V]{m: sync.Map{}}
 }
 
-func (this *Map[K, V]) Put(key K, value V) {
-	this.m.Swap(key, value)
+func (t *Map[K, V]) Put(key K, value V) {
+	t.m.Swap(key, value)
 }
 
-func (this *Map[K, V]) Swap(key K, value V) (v V, ok bool) {
-	if previous, loaded := this.m.Swap(key, value); loaded {
-		if previous != nil {
-			v = previous.(V)
-		}
-		ok = true
-	}
-	return
-}
-
-func (this *Map[K, V]) Get(key K) (v V, b bool) {
-	if e, ok := this.m.Load(key); ok {
+func (t *Map[K, V]) Get(key K) (v V, b bool) {
+	if e, ok := t.m.Load(key); ok {
 		if e != nil {
 			v = e.(V)
 		}
@@ -100,18 +105,31 @@ func (this *Map[K, V]) Get(key K) (v V, b bool) {
 	return
 }
 
-func (this *Map[K, V]) Has(key K) (ok bool) {
-	_, ok = this.m.Load(key)
+func (t *Map[K, V]) Has(key K) (ok bool) {
+	_, ok = t.m.Load(key)
 	return
 }
 
-func (this *Map[K, V]) Del(key K) (ok bool) {
-	_, ok = this.m.LoadAndDelete(key)
+func (t *Map[K, V]) Del(key K) (ok bool) {
+	_, ok = t.m.LoadAndDelete(key)
 	return
 }
 
-func (this *Map[K, V]) Range(f func(k K, v V) bool) {
-	this.m.Range(func(k, v any) bool {
+// Swap the value associated with the given key in the Map.
+// If the key exists, return the old value and true; otherwise, return the zero value and false.
+func (t *Map[K, V]) Swap(key K, value V) (_r V, loaded bool) {
+	var previous any
+	if previous, loaded = t.m.Swap(key, value); loaded && previous != nil {
+		_r = previous.(V)
+	}
+	return
+}
+
+// Range  Iterate over all elements in the Map.
+// For each element, call the provided function with the key and value.
+// The iteration stops if the function returns false.
+func (t *Map[K, V]) Range(f func(k K, v V) bool) {
+	t.m.Range(func(k, v any) bool {
 		if v != nil {
 			return f(k.(K), v.(V))
 		} else {
@@ -122,398 +140,212 @@ func (this *Map[K, V]) Range(f func(k K, v V) bool) {
 }
 
 /***********************************************************/
-
-// the big numbers come front
-type SortMap[K int | int64 | int8 | int32 | string, V any] struct {
-	l   *list.List
-	m   *Map[K, V]
-	mux *sync.RWMutex
-}
-
-func NewSortMap[K int | int64 | int8 | int32 | string, V any]() *SortMap[K, V] {
-	return &SortMap[K, V]{l: list.New(), m: NewMap[K, V](), mux: &sync.RWMutex{}}
-}
-
-func (this *SortMap[K, V]) Put(key K, value V) {
-	defer this.mux.Unlock()
-	this.mux.Lock()
-	this.m.Put(key, value)
-	this.l.PushFront(key)
-	this._swap(this.l.Front())
-}
-
-func (this *SortMap[K, V]) Get(key K) (v V, ok bool) {
-	v, ok = this.m.Get(key)
-	return
-}
-
-func (this *SortMap[K, V]) GetFrontKey() (k K, ok bool) {
-	defer this.mux.RUnlock()
-	this.mux.RLock()
-	if e := this.l.Front(); e != nil {
-		if e.Value != nil {
-			k, ok = e.Value.(K), true
-		} else {
-			ok = true
-		}
-	}
-	return
-}
-
-func (this *SortMap[K, V]) FrontForEach(f func(k K, v V) bool) {
-	defer this.mux.RUnlock()
-	this.mux.RLock()
-	for e := this.l.Front(); e != nil; e = e.Next() {
-		if e.Value != nil {
-			k := e.Value.(K)
-			if v, ok := this.m.Get(k); !ok || !f(k, v) {
-				break
-			}
-		}
-	}
-}
-
-func (this *SortMap[K, V]) BackForEach(f func(k K, v V) bool) {
-	defer this.mux.RUnlock()
-	this.mux.RLock()
-	for e := this.l.Back(); e != nil; e = e.Prev() {
-		if e.Value != nil {
-			k := e.Value.(K)
-			if v, ok := this.m.Get(k); !ok || !f(k, v) {
-				break
-			}
-		}
-	}
-}
-
-func (this *SortMap[K, V]) _swap(e *list.Element) {
-	if e != nil && e.Next() != nil && e.Value.(K) < e.Next().Value.(K) {
-		this.l.MoveAfter(e, e.Next())
-		this._swap(e)
-	}
-}
-
-func (this *SortMap[K, V]) DelAndLoadBack() (k K, v V) {
-	defer this.mux.Unlock()
-	this.mux.Lock()
-	if e := this.l.Back(); e != nil {
-		this.l.Remove(e)
-		if e.Value != nil {
-			k = e.Value.(K)
-			v, _ = this.m.Get(k)
-			this.m.Del(k)
-		}
-	}
-	return
-}
-
-func (this *SortMap[K, V]) Len() int {
-	return this.l.Len()
-}
+//
+//// SortMap the big numbers come front
+//type SortMap[K int | int64 | int8 | int32 | string, V any] struct {
+//	l   *list.List
+//	m   *Map[K, V]
+//	mux *sync.RWMutex
+//}
+//
+//func NewSortMap[K int | int64 | int8 | int32 | string, V any]() *SortMap[K, V] {
+//	return &SortMap[K, V]{l: list.New(), m: NewMap[K, V](), mux: &sync.RWMutex{}}
+//}
+//
+//func (t *SortMap[K, V]) Put(key K, value V) {
+//	defer t.mux.Unlock()
+//	t.mux.Lock()
+//	t.m.Put(key, value)
+//	t.l.PushFront(key)
+//	t._swap(t.l.Front())
+//}
+//
+//func (t *SortMap[K, V]) Get(key K) (v V, ok bool) {
+//	v, ok = t.m.Get(key)
+//	return
+//}
+//
+//func (t *SortMap[K, V]) GetFrontKey() (k K, ok bool) {
+//	defer t.mux.RUnlock()
+//	t.mux.RLock()
+//	if e := t.l.Front(); e != nil {
+//		if e.Value != nil {
+//			k, ok = e.Value.(K), true
+//		} else {
+//			ok = true
+//		}
+//	}
+//	return
+//}
+//
+//func (t *SortMap[K, V]) FrontForEach(f func(k K, v V) bool) {
+//	defer t.mux.RUnlock()
+//	t.mux.RLock()
+//	for e := t.l.Front(); e != nil; e = e.Next() {
+//		if e.Value != nil {
+//			k := e.Value.(K)
+//			if v, ok := t.m.Get(k); !ok || !f(k, v) {
+//				break
+//			}
+//		}
+//	}
+//}
+//
+//func (t *SortMap[K, V]) BackForEach(f func(k K, v V) bool) {
+//	defer t.mux.RUnlock()
+//	t.mux.RLock()
+//	for e := t.l.Back(); e != nil; e = e.Prev() {
+//		if e.Value != nil {
+//			k := e.Value.(K)
+//			if v, ok := t.m.Get(k); !ok || !f(k, v) {
+//				break
+//			}
+//		}
+//	}
+//}
+//
+//func (t *SortMap[K, V]) _swap(e *list.Element) {
+//	if e != nil && e.Next() != nil && e.Value.(K) < e.Next().Value.(K) {
+//		t.l.MoveAfter(e, e.Next())
+//		t._swap(e)
+//	}
+//}
+//
+//func (t *SortMap[K, V]) DelAndLoadBack() (k K, v V) {
+//	defer t.mux.Unlock()
+//	t.mux.Lock()
+//	if e := t.l.Back(); e != nil {
+//		t.l.Remove(e)
+//		if e.Value != nil {
+//			k = e.Value.(K)
+//			v, _ = t.m.Get(k)
+//			t.m.Del(k)
+//		}
+//	}
+//	return
+//}
+//
+//func (t *SortMap[K, V]) Len() int {
+//	return t.l.Len()
+//}
 
 /************************************************************/
-type LinkedMap[K, V any] struct {
-	l   *list.List
-	m   *Map[K, *list.Element]
-	mux *sync.Mutex
-}
 
-func NewLinkedMap[K, V any]() *LinkedMap[K, V] {
-	return &LinkedMap[K, V]{list.New(), NewMap[K, *list.Element](), &sync.Mutex{}}
-}
+// LinkedMap
+// Deprecated
+// Use LinkedHashMap instead.
+//type LinkedMap[K, V any] struct {
+//	l   *list.List
+//	m   *Map[K, *list.Element]
+//	mux *sync.Mutex
+//}
 
-func (this *LinkedMap[K, V]) Put(k K, v V) {
-	defer this.mux.Unlock()
-	this.mux.Lock()
-	if e, ok := this.m.Swap(k, this.l.PushFront([]any{k, v})); ok {
-		this.l.Remove(e)
-	}
-}
-
-func (this *LinkedMap[K, V]) Get(key K) (v V, ok bool) {
-	defer recover()
-	if e, ok := this.m.Get(key); ok {
-		return e.Value.([]any)[1].(V), ok
-	}
-	return
-}
-
-func (this *LinkedMap[K, V]) Has(key K) (ok bool) {
-	return this.m.Has(key)
-}
-
-func (this *LinkedMap[K, V]) Len() int {
-	return this.l.Len()
-}
-
-func (this *LinkedMap[K, V]) Del(key K) (ok bool) {
-	defer this.mux.Unlock()
-	this.mux.Lock()
-	if e, ok := this.m.Get(key); ok {
-		this.l.Remove(e)
-		this.m.Del(key)
-		return ok
-	}
-	return
-}
-
-func (this *LinkedMap[K, V]) Prev(key K) (k K, v V, _ok bool) {
-	defer this.mux.Unlock()
-	this.mux.Lock()
-	if e, ok := this.m.Get(key); ok {
-		if _v := e.Prev(); _v != nil && _v.Value != nil {
-			k, v = _v.Value.([]any)[0].(K), _v.Value.([]any)[1].(V)
-		}
-		_ok = true
-	} else {
-		_ok = false
-	}
-	return
-}
-
-func (this *LinkedMap[K, V]) Next(key K) (k K, v V) {
-	defer this.mux.Unlock()
-	this.mux.Lock()
-	if e, ok := this.m.Get(key); ok {
-		if _v := e.Next(); _v != nil && _v.Value != nil {
-			k, v = _v.Value.([]any)[0].(K), _v.Value.([]any)[1].(V)
-		}
-	}
-	return
-}
-
-func (this *LinkedMap[K, V]) Back() (k K) {
-	defer this.mux.Unlock()
-	this.mux.Lock()
-	if e := this.l.Back(); e != nil && e.Value != nil {
-		k = e.Value.([]any)[0].(K)
-	}
-	return
-}
-
-func (this *LinkedMap[K, V]) Front() (k K) {
-	defer recover()
-	defer this.mux.Unlock()
-	this.mux.Lock()
-	if e := this.l.Front(); e != nil && e.Value != nil {
-		k = e.Value.([]any)[0].(K)
-	}
-	return
-}
-
-func (this *LinkedMap[K, V]) BackForEach(f func(k K, v V) bool) {
-	defer recover()
-	for e := this.l.Back(); e != nil; e = e.Prev() {
-		if e.Value != nil {
-			es := e.Value.([]any)
-			if !f(es[0].(K), es[1].(V)) {
-				break
-			}
-		}
-	}
-}
-
-func (this *LinkedMap[K, V]) FrontForEach(f func(k K, v V) bool) {
-	defer recover()
-	for e := this.l.Front(); e != nil; e = e.Next() {
-		if e.Value != nil {
-			es := e.Value.([]any)
-			if !f(es[0].(K), es[1].(V)) {
-				break
-			}
-		}
-	}
-}
-
-type LimitMap[K, V any] struct {
-	m     *sync.Map
-	ch    chan K
-	limit int64
-	count int64
-}
-
-func NewLimitMap[K, V any](limit int64) *LimitMap[K, V] {
-	return &LimitMap[K, V]{&sync.Map{}, make(chan K, limit), limit, 0}
-}
-
-func (this *LimitMap[K, V]) Put(k K, v V) {
-	if _, ok := this.m.Swap(k, v); !ok {
-		if atomic.AddInt64(&this.count, 1) >= this.limit {
-			if _, ok := this.m.LoadAndDelete(<-this.ch); ok {
-				atomic.AddInt64(&this.count, -1)
-			}
-		}
-		this.ch <- k
-	}
-}
-
-func (this *LimitMap[K, V]) LoadOrStore(k K, v V) (actual V, loaded bool) {
-	if previous, ok := this.m.LoadOrStore(k, v); !ok {
-		if atomic.AddInt64(&this.count, 1) >= this.limit {
-			if _, ok := this.m.LoadAndDelete(<-this.ch); ok {
-				atomic.AddInt64(&this.count, -1)
-			}
-		}
-		this.ch <- k
-	} else {
-		if previous != nil {
-			actual = previous.(V)
-		}
-		loaded = ok
-	}
-	return
-}
-
-func (this *LimitMap[K, V]) Get(k K) (_r V, b bool) {
-	if v, ok := this.m.Load(k); ok {
-		if v != nil {
-			_r = v.(V)
-		}
-		b = ok
-	}
-	return
-}
-
-func (this *LimitMap[K, V]) Has(k K) (b bool) {
-	_, b = this.m.Load(k)
-	return
-}
-
-/***********************************************************/
-
-func int64ToBytes(n int64) (bs []byte) {
-	bs = make([]byte, 8)
-	for i := 0; i < 8; i++ {
-		bs[i] = byte(n >> (8 * (7 - i)))
-	}
-	return
-}
-
-func hash(bs []byte) uint64 {
-	return crc64.Checksum(bs, crc64.MakeTable(crc64.ECMA))
-}
-
-type Consistenthash struct {
-	replicas int
-	keys     []uint64
-	m        *Map[uint64, int64]
-	mux      *sync.RWMutex
-	_m       map[int64]int8
-}
-
-func NewConsistenthash(replicas int) (m *Consistenthash) {
-	m = &Consistenthash{
-		replicas: replicas,
-		m:        NewMap[uint64, int64](),
-		mux:      &sync.RWMutex{},
-		_m:       map[int64]int8{},
-	}
-	return m
-}
-
-func (this *Consistenthash) Add(keys ...int64) {
-	this.mux.Lock()
-	defer this.mux.Unlock()
-	for _, key := range keys {
-		if _, ok := this._m[key]; !ok {
-			this._m[key] = 0
-			for i := 0; i < this.replicas; i++ {
-				buf := NewBufferByPool()
-				buf.Write(int64ToBytes(int64(i)))
-				buf.Write(int64ToBytes(key))
-				h := hash(buf.Bytes())
-				buf.Free()
-				this.keys = append(this.keys, h)
-				this.m.Put(h, key)
-			}
-		}
-	}
-	sort.Slice(this.keys, func(i, j int) bool { return this.keys[i] < this.keys[j] })
-}
-
-func (this *Consistenthash) Get(value int64) (node int64, ok bool) {
-	this.mux.RLock()
-	defer this.mux.RUnlock()
-	if this.keys == nil {
-		return
-	}
-	keyu64 := hash(int64ToBytes(value))
-	idx := sort.Search(len(this.keys), func(i int) bool { return this.keys[i] >= keyu64 })
-	if idx >= len(this.keys) {
-		idx = 0
-	}
-	node, ok = this.m.Get(this.keys[idx])
-	return
-}
-
-func (this *Consistenthash) GetStr(value string) (node int64, ok bool) {
-	this.mux.RLock()
-	defer this.mux.RUnlock()
-	if this.keys == nil {
-		return
-	}
-	keyu64 := hash([]byte(value))
-	idx := sort.Search(len(this.keys), func(i int) bool { return this.keys[i] >= keyu64 })
-	if idx >= len(this.keys) {
-		idx = 0
-	}
-	node, ok = this.m.Get(this.keys[idx])
-	return
-}
-
-func (this *Consistenthash) GetNextNodeStr(value string, step int) (nodes []int64, ok bool) {
-	this.mux.RLock()
-	defer this.mux.RUnlock()
-	if this.keys == nil {
-		return
-	}
-	keyu64 := hash([]byte(value))
-	idx := sort.Search(len(this.keys), func(i int) bool { return this.keys[i] >= keyu64 })
-	if idx >= len(this.keys) {
-		idx = 0
-	}
-	n1, ok := this.m.Get(this.keys[idx])
-	m := map[int64]int8{}
-	nodes = make([]int64, 0)
-	for idx < len(this.keys)-1 {
-		idx++
-		if node, ok := this.m.Get(this.keys[idx]); ok && node != n1 {
-			if _, ok := m[node]; !ok {
-				m[node] = 0
-				nodes = append(nodes, node)
-			}
-		}
-		if len(nodes) == step {
-			return
-		}
-	}
-	return
-}
-
-func (this *Consistenthash) Del(key int64) {
-	this.mux.Lock()
-	defer this.mux.Unlock()
-	if _, ok := this._m[key]; ok {
-		this.keys = this.keys[:0]
-		this.m.Range(func(k uint64, v int64) bool {
-			if v == key {
-				this.m.Del(k)
-			} else {
-				this.keys = append(this.keys, k)
-			}
-			return true
-		})
-		sort.Slice(this.keys, func(i, j int) bool { return this.keys[i] < this.keys[j] })
-		delete(this._m, key)
-	}
-}
-
-func (this *Consistenthash) Nodes() (_r []int64) {
-	this.mux.Lock()
-	defer this.mux.Unlock()
-	_r = make([]int64, 0)
-	for k := range this._m {
-		_r = append(_r, k)
-	}
-	return
-}
+//// NewLinkedMap
+//// Deprecated
+//// Use NewLinkedHashMap instead.
+//func NewLinkedMap[K, V any]() *LinkedMap[K, V] {
+//	return &LinkedMap[K, V]{list.New(), NewMap[K, *list.Element](), &sync.Mutex{}}
+//}
+//
+//func (t *LinkedMap[K, V]) Put(k K, v V) {
+//	defer t.mux.Unlock()
+//	t.mux.Lock()
+//	if e, ok := t.m.Swap(k, t.l.PushFront([]any{k, v})); ok {
+//		t.l.Remove(e)
+//	}
+//}
+//
+//func (t *LinkedMap[K, V]) Get(key K) (v V, ok bool) {
+//	defer recover()
+//	if e, ok := t.m.Get(key); ok {
+//		return e.Value.([]any)[1].(V), ok
+//	}
+//	return
+//}
+//
+//func (t *LinkedMap[K, V]) Has(key K) (ok bool) {
+//	return t.m.Has(key)
+//}
+//
+//func (t *LinkedMap[K, V]) Len() int {
+//	return t.l.Len()
+//}
+//
+//func (t *LinkedMap[K, V]) Del(key K) (ok bool) {
+//	defer t.mux.Unlock()
+//	t.mux.Lock()
+//	if e, ok := t.m.Get(key); ok {
+//		t.l.Remove(e)
+//		t.m.Del(key)
+//		return ok
+//	}
+//	return
+//}
+//
+//func (t *LinkedMap[K, V]) Prev(key K) (k K, v V, _ok bool) {
+//	defer t.mux.Unlock()
+//	t.mux.Lock()
+//	if e, ok := t.m.Get(key); ok {
+//		if _v := e.Prev(); _v != nil && _v.Value != nil {
+//			k, v = _v.Value.([]any)[0].(K), _v.Value.([]any)[1].(V)
+//		}
+//		_ok = true
+//	} else {
+//		_ok = false
+//	}
+//	return
+//}
+//
+//func (t *LinkedMap[K, V]) Next(key K) (k K, v V) {
+//	defer t.mux.Unlock()
+//	t.mux.Lock()
+//	if e, ok := t.m.Get(key); ok {
+//		if _v := e.Next(); _v != nil && _v.Value != nil {
+//			k, v = _v.Value.([]any)[0].(K), _v.Value.([]any)[1].(V)
+//		}
+//	}
+//	return
+//}
+//
+//func (t *LinkedMap[K, V]) Back() (k K) {
+//	defer t.mux.Unlock()
+//	t.mux.Lock()
+//	if e := t.l.Back(); e != nil && e.Value != nil {
+//		k = e.Value.([]any)[0].(K)
+//	}
+//	return
+//}
+//
+//func (t *LinkedMap[K, V]) Front() (k K) {
+//	defer recover()
+//	defer t.mux.Unlock()
+//	t.mux.Lock()
+//	if e := t.l.Front(); e != nil && e.Value != nil {
+//		k = e.Value.([]any)[0].(K)
+//	}
+//	return
+//}
+//
+//func (t *LinkedMap[K, V]) BackForEach(f func(k K, v V) bool) {
+//	defer recover()
+//	for e := t.l.Back(); e != nil; e = e.Prev() {
+//		if e.Value != nil {
+//			es := e.Value.([]any)
+//			if !f(es[0].(K), es[1].(V)) {
+//				break
+//			}
+//		}
+//	}
+//}
+//
+//func (t *LinkedMap[K, V]) FrontForEach(f func(k K, v V) bool) {
+//	defer recover()
+//	for e := t.l.Front(); e != nil; e = e.Next() {
+//		if e.Value != nil {
+//			es := e.Value.([]any)
+//			if !f(es[0].(K), es[1].(V)) {
+//				break
+//			}
+//		}
+//	}
+//}
