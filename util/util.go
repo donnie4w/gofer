4cc2c9ffff9975d8cb077092976081b911dde324
donnie4w <donnie4w@gmail.com>
2024-10-25 22:49:22 +0800

    util

--- util/util.go
+++ util/util.go
@@ -1,5 +1,8 @@
-// Copyright (c) , donnie <donnie4w@gmail.com>
+// Copyright (c) 2023, donnie <donnie4w@gmail.com>
 // All rights reserved.
+// Use of t source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
 // github.com/donnie4w/gofer/util
 
 package util
