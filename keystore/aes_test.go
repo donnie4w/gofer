17ae989f3f396de0e55a9a0356b339ca1dc09bd7
donnie4w <donnie4w@gmail.com>
2024-10-25 22:48:21 +0800

    keystore

--- keystore/aes_test.go
+++ keystore/aes_test.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2023, donnie <donnie4w@gmail.com>
+// All rights reserved.
+// Use of t source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 package keystore
 
 import (
@@ -18,7 +22,7 @@ func BenchmarkAES(b *testing.B) {
 	a := NewAes("ajfijeifanb", "iejfefvvvvvvvvefa")
 	for i := 0; i < b.N; i++ {
 		s := "1234567"
-		s = s +  fmt.Sprint(i)
+		s = s + fmt.Sprint(i)
 		bs, _ := a.EncrypterCbc([]byte(s))
 		b, _ := a.DecrypterCbc(bs)
 		if string(b) != s {
--- keystore/bin.go
+++ keystore/bin.go
@@ -1,7 +1,9 @@
-// Copyright (c) , donnie <donnie4w@gmail.com>
+// Copyright (c) 2023, donnie <donnie4w@gmail.com>
 // All rights reserved.
+// Use of t source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 //
-// github.com/donnie4w/tim/keystore
+// github.com/donnie4w/gofer/keystore
 
 package keystore
 
