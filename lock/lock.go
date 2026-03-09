d13baf4386ac34a8fd867dd6402f8d908fdac223
donnie4w <donnie4w@gmail.com>
2026-03-09 12:23:45 +0800

    Update lock.go

--- lock/lock.go
+++ lock/lock.go
@@ -9,165 +9,162 @@ package lock
 
 import (
 	"errors"
-	"fmt"
 	"sync"
-	"sync/atomic"
 	"time"
 
-	"github.com/donnie4w/gofer/hashmap"
 	"github.com/donnie4w/gofer/util"
 )
 
 // Numlock provides a set of mutex locks based on integer keys.
 type Numlock struct {
-	lockm  *hashmap.Map[int64, *sync.Mutex] // Map for storing mutexes indexed by keys
-	muxNum int                              // Number of mutexes
+	locks  []*sync.Mutex
+	muxNum uint64
 }
 
 // NewNumLock initializes and returns a new Numlock instance.
 func NewNumLock(muxNum int) *Numlock {
-	ml := &Numlock{hashmap.NewMap[int64, *sync.Mutex](), muxNum}
-	for i := 0; i < muxNum; i++ {
-		ml.lockm.Put(int64(i), &sync.Mutex{}) // Initialize mutexes
+	if muxNum <= 0 {
+		panic("muxNum must be > 0")
 	}
-	return ml
+	locks := make([]*sync.Mutex, muxNum)
+	for i := range locks {
+		locks[i] = &sync.Mutex{}
+	}
+	return &Numlock{locks: locks, muxNum: uint64(muxNum)}
+}
+
+func (nl *Numlock) index(key int64) uint64 {
+	return uint64(key) % nl.muxNum
 }
 
 // Lock acquires the lock associated with the given key.
 func (nl *Numlock) Lock(key int64) *sync.Mutex {
-	l, _ := nl.lockm.Get(int64(uint64(key) % uint64(nl.muxNum)))
+	l := nl.locks[nl.index(key)]
 	l.Lock()
 	return l
 }
 
 // TryLock try to acquire the lock associated with the given key.
 func (nl *Numlock) TryLock(key int64) (*sync.Mutex, bool) {
-	l, _ := nl.lockm.Get(int64(uint64(key) % uint64(nl.muxNum)))
+	l := nl.locks[nl.index(key)]
 	if l.TryLock() {
 		return l, true
-	} else {
-		return nil, false
 	}
+	return nil, false
 }
 
 // Unlock releases the lock associated with the given key.
 func (nl *Numlock) Unlock(key int64) {
-	l, _ := nl.lockm.Get(int64(uint64(key) % uint64(nl.muxNum)))
-	l.Unlock()
+	nl.locks[nl.index(key)].Unlock()
 }
 
 // Strlock provides a set of read-write mutex locks based on string keys.
 type Strlock struct {
-	lockm  *hashmap.Map[int64, *sync.RWMutex] // Map for storing read-write mutexes indexed by keys
-	muxNum int                                // Number of read-write mutexes
+	locks  []*sync.RWMutex
+	muxNum uint64
 }
 
 // NewStrlock initializes and returns a new Strlock instance.
 func NewStrlock(muxNum int) *Strlock {
-	ml := &Strlock{hashmap.NewMap[int64, *sync.RWMutex](), muxNum}
-	for i := 0; i < muxNum; i++ {
-		ml.lockm.Put(int64(i), &sync.RWMutex{}) // Initialize read-write mutexes
+	if muxNum <= 0 {
+		panic("muxNum must be > 0")
 	}
-	return ml
+	locks := make([]*sync.RWMutex, muxNum)
+	for i := range locks {
+		locks[i] = &sync.RWMutex{}
+	}
+	return &Strlock{locks: locks, muxNum: uint64(muxNum)}
+}
+
+func (sl *Strlock) index(key string) uint64 {
+	u := util.Hash64([]byte(key))
+	return u % sl.muxNum
 }
 
 // Lock acquires the write lock associated with the given key.
 func (sl *Strlock) Lock(key string) *sync.RWMutex {
-	u := util.Hash64([]byte(key)) // Calculate hash of key
-	l, _ := sl.lockm.Get(int64(u % uint64(sl.muxNum)))
+	l := sl.locks[sl.index(key)]
 	l.Lock()
 	return l
 }
 
 // TryLock try to acquire the write lock associated with the given key.
 func (sl *Strlock) TryLock(key string) (*sync.RWMutex, bool) {
-	u := util.Hash64([]byte(key)) // Calculate hash of key
-	l, _ := sl.lockm.Get(int64(u % uint64(sl.muxNum)))
+	l := sl.locks[sl.index(key)]
 	if l.TryLock() {
 		return l, true
-	} else {
-		return nil, false
 	}
+	return nil, false
 }
 
 // Unlock releases the write lock associated with the given key.
 func (sl *Strlock) Unlock(key string) {
-	u := util.Hash64([]byte(key)) // Calculate hash of key
-	l, _ := sl.lockm.Get(int64(u % uint64(sl.muxNum)))
-	l.Unlock()
+	sl.locks[sl.index(key)].Unlock()
 }
 
 // RLock acquires the read lock associated with the given key.
 func (sl *Strlock) RLock(key string) *sync.RWMutex {
-	u := util.Hash64([]byte(key)) // Calculate hash of key
-	l, _ := sl.lockm.Get(int64(u % uint64(sl.muxNum)))
+	l := sl.locks[sl.index(key)]
 	l.RLock()
 	return l
 }
 
 // TryRLock try to acquire the read lock associated with the given key.
 func (sl *Strlock) TryRLock(key string) (*sync.RWMutex, bool) {
-	u := util.Hash64([]byte(key)) // Calculate hash of key
-	l, _ := sl.lockm.Get(int64(u % uint64(sl.muxNum)))
+	l := sl.locks[sl.index(key)]
 	if l.TryRLock() {
 		return l, true
-	} else {
-		return nil, false
 	}
+	return nil, false
 }
 
 // RUnlock releases the read lock associated with the given key.
 func (sl *Strlock) RUnlock(key string) {
-	u := util.Hash64([]byte(key)) // Calculate hash of key
-	l, _ := sl.lockm.Get(int64(u % uint64(sl.muxNum)))
-	l.RUnlock()
+	sl.locks[sl.index(key)].RUnlock()
 }
 
 // LimitLock limits the number of concurrent operations and can enforce timeouts.
 type LimitLock struct {
-	ch      chan int      // Channel used for limiting concurrency
-	count   int64         // Atomic counter for tracking lock acquisitions
-	_count  int64         // Atomic counter for tracking lock releases (unused)
-	timeout time.Duration // Timeout duration
+	ch chan int
+	//count   int64
+	//_count  int64
+	timeout time.Duration
 }
 
 // NewLimitLock initializes and returns a new LimitLock instance.
 func NewLimitLock(limit int, timeout time.Duration) *LimitLock {
-	ch := make(chan int, limit) // Create a buffered channel
-	return &LimitLock{ch: ch, timeout: timeout}
+	return &LimitLock{
+		ch:      make(chan int, limit),
+		timeout: timeout,
+	}
 }
 
 // Lock acquires a lock with a timeout.
-func (ll *LimitLock) Lock() (err error) {
+func (ll *LimitLock) Lock() error {
+	timer := time.NewTimer(ll.timeout)
+	defer timer.Stop()
+
 	select {
-	case <-time.After(ll.timeout): // Wait until timeout
-		err = errors.New("timeout")
-	case ll.ch <- 1: // Acquire lock
-		atomic.AddInt64(&ll.count, 1) // Increment lock count
+	case ll.ch <- 1:
+		//atomic.AddInt64(&ll.count, 1)
+		return nil
+	case <-timer.C:
+		return errors.New("timeout")
 	}
-	return
 }
 
 // Unlock releases a lock.
 func (ll *LimitLock) Unlock() {
-	<-ll.ch                        // Release lock
-	atomic.AddInt64(&ll._count, 1) // Increment release count (unused)
+	<-ll.ch
+	//atomic.AddInt64(&ll._count, 1)
 }
 
 // Cc returns the current concurrency count.
-func (ll *LimitLock) Cc() int64 {
-	return ll.count - ll._count // Difference between acquisitions and releases (always zero)
+func (ll *LimitLock) Cc() int {
+	return len(ll.ch)
 }
 
 // LockCount returns the total number of lock acquisitions.
-func (ll *LimitLock) LockCount() int64 {
-	return atomic.LoadInt64(&ll.count) // Current lock acquisition count
-}
-
-func recoverpanic(err *error) {
-	if r := recover(); r != nil {
-		if err != nil {
-			*err = fmt.Errorf("%v", r)
-		}
-	}
-}
+//func (ll *LimitLock) LockCount() int64 {
+//	return atomic.LoadInt64(&ll.count)
+//}
